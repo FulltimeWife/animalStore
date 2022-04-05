e7b5b40dc5f9b2b231b552db7fce9f62c0c9a92b
Jessica <jessicabyrne812@gmail.com>
2022-04-05 14:58:23 +0100

    homescreen.js still not displaying anything.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Product = () => {
     return <div className="product">
-      <img src="" alt=""></img>
+      <img src="https://i.imgur.com/ElxAyoL.png" alt="sleepy panda"></img>
 
-      <div className=""product__info>
-        <p className="info__name"></p>
-        <p className="info__description"></p>
+      <div className="product__info">
+        <p className="info__name">Sleepy Panda</p>
+        <p className="info__description">He's just a lil snoozer</p>
         <p className="info__price">€345.99</p>
 
-        <Link to={`/product/${1111}`} className="info__button"></Link>
+        <Link to={`/product/${1}`} className="info__button"></Link>
       </div>
     </div>
 }
