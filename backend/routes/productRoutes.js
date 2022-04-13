c31eb2dff2992b95000fd78e8bc81c557b07f9ee
Jessica <jessicabyrne812@gmail.com>
2022-04-13 03:33:35 +0100

    Fleshing out the start of account creation.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,4 +15,22 @@ router.get("/", getAllProducts)
 
 router.get("/:id", getProductByID);
 
+/*
+//@desc     GET account information from the DB.
+//@route    GET /accounts/:id
+//@access   Private.
+
+router.get("/accounts/:id", getAccountInformation)
+
+//@desc     GET account information from the DB.
+//@route    GET /accounts/signin
+//@access   Private.
+
+router.get("/accounts/signin", getAccountInformation)
+
+//@desc     POST new account information to the DB
+//@route    POST /account/signup
+//@access   Private.
+router.put("/accounts/signup, createAccount")
+*/
 module.exports = router;
